4d7d402129717eb0d34ce981242adbff1834bc9c
noy2611 <noyr2611@gmail.com>
2024-01-22 17:43:57 +0200

    Create GlobalStyles.js

---
+++ EMSquad/GlobalStyles.js
@@ -0,0 +1,13 @@
+// globalStyles.js
+import { StyleSheet } from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 0,
+    margin: 0,
+  },
+  // Add more global styles as needed
+});
+
+export default globalStyles;
