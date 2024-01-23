d43bea8d0e9360f065f282d33a0220264e816074
Roey ben harush <roey1234r@gmail.com>
2024-01-23 10:15:34 +0200

    Server.js + handleLogin()

--- EMSquad/App.js
+++ EMSquad/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import LoginScreen from './Login'; 
+import HomeScreen from './Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <LoginScreen />
+      {/* <HomeScreen /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2C2C2D',
+    backgroundColor: '#090909',
     alignItems: 'center',
     justifyContent: 'center',
   },
