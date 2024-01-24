61f383e2283eead03be9b5ed615b54c600812ea5
Roey ben harush <roey1234r@gmail.com>
2024-01-24 17:33:44 +0200

    reorder_files + Navbar.js

---
+++ EMSquad/component/SoundUtils.js
@@ -0,0 +1,38 @@
+import { Audio } from "expo-av";
+
+let alarmSound;
+
+const loadSound = async () => {
+  try {
+    alarmSound = new Audio.Sound();
+    await alarmSound.loadAsync(
+      require("../assets/security-alarm.mp3")
+    );
+  } catch (error) {
+    console.error("Error loading sound:", error);
+  }
+};
+
+const playSound = async () => {
+  try {
+    if (alarmSound) {
+      await alarmSound.playAsync();
+    }
+  } catch (error) {
+    console.error("Error playing sound:", error);
+  }
+};
+
+const stopSound = async () => {
+  try {
+    if (alarmSound) {
+      await alarmSound.stopAsync();
+      await alarmSound.unloadAsync();
+      alarmSound = null; // Reset the sound instance
+    }
+  } catch (error) {
+    console.error("Error stopping sound:", error);
+  }
+};
+
+export { loadSound, playSound, stopSound };
