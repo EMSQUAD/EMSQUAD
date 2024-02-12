cf4ffabc0d0367fb7a076383294b66828583ab00
noy2611 <noyr2611@gmail.com>
2024-02-12 19:53:06 +0200

    db-model-user

--- models/user.model.js
+++ models/user.model.js
@@ -1,1 +1,20 @@
-const MongoStorage = require('./db/MongoStorage');
+const MongoStorage = require("./db/MongoStorage");
+
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    id: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: {type: String,required: true },
+    phone: {type: String,required: true},
+    type_user: {type: String,required: true},
+    password: {type: String,required: true},
+    status_ability: {type: String,required: true},
+    Image: {type: String,required: true},
+  },
+  {collation: "coll-ems-user", versionKey: false},
+);
+
+module.exports = model("User-ems", userSchema);
