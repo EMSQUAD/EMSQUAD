1e3a84f0ebaab7179e86cfc9b1391525dfbb95cb
Alon A. Rabinowitz <alonrab@outlook.com>
2024-02-17 21:08:26 +0200

    Revert "Merge branch 'main' into noybranch"
    This reverts commit 673a1f503fc6b30536cd4d542064bf90682af11e, reversing
    changes made to 33eda928492d1ea5731f9b6b71b4e6d9db62f5ca.

--- App.jsx
+++ App.jsx
@@ -1,72 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import TalkButton from './component/TalkButton';
-import UserList from './component/UserList';
-import WokitokiService from './component/WokitokiService';
-import RecordRTC from 'recordrtc';
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+// import WalkieTalkie from './component/walkieTalkie';
+import HomeScreen from './component/Home';
 
 const App = () => {
-  const [selectedUser, setSelectedUser] = useState('');
-  const [isRecording, setIsRecording] = useState(false);
-  const [recorder, setRecorder] = useState(null);
-  const [audioBlob, setAudioBlob] = useState(null);
-
-  useEffect(() => {
-    WokitokiService.init();
-  }, []);
-
-  const onStartRecording = () => {
-    console.log('Start recording');
-    setIsRecording(true);
-
-    const recorder = new RecordRTC({
-      type: 'audio',
-      recorderType: StereoAudioRecorder,
-      numberOfAudioChannels: 2,
-      sampleRate: 44100,
-    });
-
-    recorder.startRecording();
-    setRecorder(recorder);
-  };
-
-  const onStopRecording = () => {
-    console.log('Stop recording');
-    setIsRecording(false);
-
-    recorder.stopRecording(() => {
-      const blob = recorder.getBlob();
-      setAudioBlob(blob);
-
-      // ... (שמירת הקובץ בשרת, שליחה למשתמש אחר וכו')
-    });
-  };
-
-  const onPlayRecording = () => {
-    console.log('Play recording');
-
-    const audio = new Audio(audioBlob);
-    audio.play();
-  };
-
-  const onUserSelect = (user) => {
-    console.log('User selected:', user);
-    setSelectedUser(user);
-  };
-
   return (
-    <div>
-      <TalkButton
-        isRecording={isRecording}
-        onRecordStart={onStartRecording}
-        onRecordStop={onStopRecording}
-      />
-      <UserList onUserSelect={onUserSelect} />
-      <View style={styles.container}>
-        <Button title="הקלט" onPress={onStartRecording} />
-        <Button title="עצור" onPress={onStopRecording} />
-        <Button title="נגן" onPress={onPlayRecording} />
-      </View>
-    </div>
+    <SafeAreaView style={styles.container}>
+      {/* <WalkieTalkie /> */
+      <HomeScreen />}
+    </SafeAreaView>
   );
 };
 
