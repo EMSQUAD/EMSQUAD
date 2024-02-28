2ad6231ea88a13569150defe6a2bb816321b77f5
Roey Ben Harush <Roey1234r@gmail.com>
2024-02-28 04:19:58 +0200

    trying to login

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -9,91 +9,79 @@ import {
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./component/Home.component";
-
-// import NavBar from "./component/Navbar";
-// import Header from "./component/Header";
 import UserListComponent from "./component/DisplayUsers.component";
 import EventListComponent from "./component/DisplayEvents.component";
 import WalkieTalkiePTT from "./component/walkieTalkie.component";
 import LoginScreen from "./component/Login";
-import React, { useEffect } from "react";
+
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
-    // console.log('App height:', Dimensions.get('window').height);
+    // Check authentication status here and update isLoggedIn accordingly
+    // For simplicity, I'm assuming isLoggedIn changes based on user login/logout
   }, []);
+
   return (
-    // <SafeAreaView style={styles.container}>
     <View style={styles.container}>
       <StatusBar hidden translucent backgroundColor="transparent" />
       <NavigationContainer>
         <Stack.Navigator>
-{/* 
-          <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{
-              header: () => null, // Hide the header
-            }}
-          /> */}
-
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-              header: () => null, // Hide the header
-            }}
-          />
-          <Stack.Screen
-            name="Users"
-            component={UserListComponent}
-            options={{
-              headerShown: true,
-              headerStyle: {
-                backgroundColor: "black",
-              },
-              headerTitle: "צאטים",
-              headerTitleStyle: {
-                color: "white",
-                fontSize: 20,
-                fontWeight: "bold",
-              },
-              headerBackTitle: "חזור",
-            }}
-          />
-
-          <Stack.Screen
-            name="Events"
-            component={EventListComponent}
-            options={{
-              headerShown: true,
-              headerStyle: {
-                backgroundColor: "black",
-              },
-              headerTitle: "אירועים",
-              headerTitleStyle: {
-                color: "white",
-                fontSize: 20,
-                fontWeight: "bold",
-              },
-              headerBackTitle: "חזור",
-            }}
-          />
-
-          <Stack.Screen
-            name="WalkieTalkie"
-            component={WalkieTalkiePTT}
-            options={{
-              header: () => null, // Hide the header
-            }}
-          />
+          {isLoggedIn ? (
+            <>
+              <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="Users"
+                component={UserListComponent}
+                options={{
+                  headerShown: true,
+                  headerStyle: { backgroundColor: "black" },
+                  headerTitle: "צאטים",
+                  headerTitleStyle: {
+                    color: "white",
+                    fontSize: 20,
+                    fontWeight: "bold",
+                  },
+                  headerBackTitle: "חזור",
+                }}
+              />
+              <Stack.Screen
+                name="Events"
+                component={EventListComponent}
+                options={{
+                  headerShown: true,
+                  headerStyle: { backgroundColor: "black" },
+                  headerTitle: "אירועים",
+                  headerTitleStyle: {
+                    color: "white",
+                    fontSize: 20,
+                    fontWeight: "bold",
+                  },
+                  headerBackTitle: "חזור",
+                }}
+              />
+              <Stack.Screen
+                name="WalkieTalkie"
+                component={WalkieTalkiePTT}
+                options={{ headerShown: false }}
+              />
+            </>
+          ) : (
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+          )}
         </Stack.Navigator>
       </NavigationContainer>
-      {/* <Header /> */}
-      {/* <NavBar /> */}
-      {/* </SafeAreaView> */}
     </View>
   );
 };
