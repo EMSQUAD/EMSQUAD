e69dad77c214efcd7f51d162218e47b597838ac9
Alon A. Rabinowitz <alonrab@outlook.com>
2024-03-01 17:33:28 +0200

    merge from main

--- component/walkieTalkie.component.jsx
+++ component/walkieTalkie.component.jsx
@@ -1,6 +1,7 @@
 import { React, useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Button, Platform } from 'react-native';
 import { Audio } from 'expo-av';
+import NearbyChat from './D2D_comunication/local_messege';
 
 const WalkieTalkiePTT = () => {
     const [recording, setRecording] = useState(null);
@@ -95,6 +96,7 @@ const WalkieTalkiePTT = () => {
         if (Platform.OS === 'ios') {
             setAllowsRecordingIOS(false);
         }
+        await NearbyChat.sendMessage('Hello');
     };
 
     const playRecording = async () => {
