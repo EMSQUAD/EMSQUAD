236323e71a5f9e554d1c5b267b79f05fdbcf10ac
noy2611 <noyr2611@gmail.com>
2024-03-31 04:09:36 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,24 @@
+// module.exports = function(api) {
+//   api.cache(true);
+
+//   const presets = [['babel-preset-expo']
+//   ["module:metro-react-native-babel-preset"]];
+
+//   const plugins = [
+//     // Explicitly set the "loose" option for these plugins to silence the warning
+//     ["@babel/plugin-proposal-class-properties", { "loose": true }],
+//     ["@babel/plugin-proposal-private-methods", { "loose": true }],
+//     ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+//   ];
+
+//   return {
+//     presets,
+//     plugins
+//   };
+// };
 module.exports = function(api) {
   api.cache(true);
-
-  const presets = [['babel-preset-expo']
-  ["module:metro-react-native-babel-preset"]];
-
-  const plugins = [
-    // Explicitly set the "loose" option for these plugins to silence the warning
-    ["@babel/plugin-proposal-class-properties", { "loose": true }],
-    ["@babel/plugin-proposal-private-methods", { "loose": true }],
-    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
-  ];
-
   return {
-    presets,
-    plugins
+    presets: ['babel-preset-expo'],
   };
 };
